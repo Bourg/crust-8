195d56b73ec1adea5572b1c199417adf569a1354
Austin Bourgerie <austin@bourg.me>
2022-05-25 16:58:51 -0400

    Implement storexnn

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,15 +1,44 @@
 use Instruction::*;
 
 pub enum Instruction {
-    ADD { register: u8, amount: u8 },
+    // 6XNN
+    StoreXNN { register: u8, value: u8 },
+    // 7XNN
+    AddXNN { register: u8, value: u8 },
+    // 8XY0
+    StoreXY { target: u8, source: u8 },
+    // 8XY4
+    AddXY { target: u8, source: u8 },
+    // 8XY5
+    SubXY { target: u8, source: u8 },
+    // 8XY7
+    // TODO better naming
+    SUBXYReverse { target: u8, source: u8 },
 }
 
 impl Instruction {
     pub fn to_bytes(&self) -> [u8; 2] {
         match self {
-            // 7XNN
-            ADD { register, amount } => [u4_to_u8(7, *register), *amount],
+            StoreXNN {
+                register,
+                value: amount,
+            } => [u4_to_u8(6, *register), *amount],
+            AddXNN {
+                register,
+                value: amount,
+            } => [u4_to_u8(7, *register), *amount],
+            StoreXY {
+                target: target_register,
+                source: source_register,
+            } => [u4_to_u8(8, *target_register), u4_to_u8(*source_register, 0)],
+            _ => panic!("Not supported"), // Do we even need to_bytes?
         }
+    }
+
+    pub fn to_u16(&self) -> u16 {
+        let bytes = self.to_bytes();
+
+        ((bytes[0] as u16) << 8) + bytes[1] as u16
     }
 }
 
@@ -23,6 +52,26 @@ fn u4_to_u8(most_significant: u8, least_significant: u8) -> u8 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_to_bytes() {
+        assert_eq!(
+            0x6ABC,
+            StoreXNN {
+                register: 0xA,
+                value: 0xBC,
+            }
+            .to_u16()
+        );
+        assert_eq!(
+            0x8370,
+            StoreXY {
+                target: 3,
+                source: 7,
+            }
+            .to_u16()
+        )
+    }
 
     #[test]
     fn test_u4_to_u8() {
--- src/machine.rs
+++ src/machine.rs
@@ -1,4 +1,4 @@
-use crate::instruction;
+use crate::instruction::Instruction;
 use crate::memory;
 use crate::register;
 
@@ -15,61 +15,99 @@ impl Machine {
         }
     }
 
-    pub fn step(&mut self, instruction: instruction::Instruction) {
+    pub fn step(&mut self, instruction: &Instruction) {
         match instruction {
-            instruction::Instruction::ADD { register, amount } => {
-                let (value, carry) = self.registers.v[register as usize].overflowing_add(amount);
+            Instruction::StoreXNN { register, value } => {
+                self.registers.set_register(*register, *value);
+            }
+            Instruction::AddXNN { register, value } => {
+                let (value, carry) = self.registers.v[*register as usize].overflowing_add(*value);
 
-                self.registers.set_register(register, value);
+                self.registers.set_register(*register, value);
+                // TODO does this modify the carry, or only register-to-register addition?
                 self.registers.set_flag(if carry { 1 } else { 0 })
 
                 // TODO increment the program count
             }
+            _ => panic!("Unsupported"),
         }
+    }
+
+    pub fn step_many<'a, T>(&mut self, instructions: T)
+    where
+        T: IntoIterator<Item = &'a Instruction>,
+    {
+        instructions
+            .into_iter()
+            .for_each(|instruction| self.step(instruction))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::instruction::Instruction::*;
 
     #[test]
-    fn add() {
+    fn store_xnn() {
         let mut machine = Machine::new();
 
-        let register = 5u8;
+        machine.step_many(&[
+            StoreXNN {
+                register: 0x7,
+                value: 77,
+            },
+            StoreXNN {
+                register: 0x5,
+                value: 22,
+            },
+            StoreXNN {
+                register: 0xF,
+                value: 123,
+            },
+            StoreXNN {
+                register: 0x5,
+                value: 23,
+            },
+        ]);
+
+        assert_eq!(
+            [0, 0, 0, 0, 0, 23, 0, 77, 0, 0, 0, 0, 0, 0, 0, 123],
+            machine.registers.v
+        );
+    }
+
+    #[test]
+    fn add_xnn() {
+        let mut machine = Machine::new();
+
+        let register = 0x5;
 
         assert_eq!(0, machine.registers.get_register(register));
 
         // A first add should work and not overflow
-        machine.step(instruction::Instruction::ADD {
+        machine.step(&AddXNN {
             register,
-            amount: 106,
+            value: 106,
         });
         assert_eq!(106, machine.registers.get_register(register));
         assert_eq!(0, machine.registers.get_flag());
 
         // A second add just below the limit should also not overflow
-        machine.step(instruction::Instruction::ADD {
+        machine.step(&Instruction::AddXNN {
             register,
-            amount: 149,
+            value: 149,
         });
         assert_eq!(255, machine.registers.get_register(register));
         assert_eq!(0, machine.registers.get_flag());
 
         // Add one more and it should overflow
-        machine.step(instruction::Instruction::ADD {
-            register,
-            amount: 1,
-        });
+        machine.step(&AddXNN { register, value: 1 });
         assert_eq!(0, machine.registers.get_register(register));
         assert_eq!(1, machine.registers.get_flag());
 
         // Add one last time and the flag should reset
-        machine.step(instruction::Instruction::ADD {
-            register,
-            amount: 3,
-        });
+        machine.step(&AddXNN { register, value: 3 });
         assert_eq!(3, machine.registers.get_register(register));
         assert_eq!(0, machine.registers.get_flag());
     }
